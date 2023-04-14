9a5edea6dbbcb564a94b4a42879fc45333f895fa
Tamino <49478749+Nono-04@users.noreply.github.com>
2023-04-14 13:15:08 +0200

    fixed tab error

--- main.py
+++ main.py
@@ -92,13 +92,13 @@ class Bot(commands.Bot):
                 oldData["live"] = isCurrentlyLive
 
             if oldData["live"] != isCurrentlyLive:
-                try:
-                    await send_embed_webhook(chatMessagesWebhook, "Stream started", f"{streamer} is live!", 0x00ff00)
-                    await send_message_webhook(chatMessagesWebhook, f"{livestreamData.thumbnail_url.replace('{width}', '1920').replace('{height}', '1080')}")
-               	except Exception as e:
-                	print(e)
-                    pass
-                oldData["live"] = isCurrentlyLive
+                if isCurrentlyLive:
+                    try:
+                        await send_embed_webhook(chatMessagesWebhook, "Stream started", f"{streamer} is live!", 0x00ff00)
+                        await send_message_webhook(chatMessagesWebhook, f"{livestreamData.thumbnail_url.replace('{width}', '1920').replace('{height}', '1080')}")
+                    except Exception as e:
+                        print(e)
+                    oldData["live"] = isCurrentlyLive
 
         if oldData.get("title") is None:
             oldData["title"] = streamerData.title
